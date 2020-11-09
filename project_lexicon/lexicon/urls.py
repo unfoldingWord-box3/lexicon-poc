eedcda08110c3a436c8000e62875fd4d35aec27e
J. de Joode <10449733+jdejoode@users.noreply.github.com>
2020-11-09 14:14:08 +0100

    Create first views

--- project_lexicon/lexicon/urls.py
+++ project_lexicon/lexicon/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import view_entry, demo_entry
+from .views import view_entry, demo_entry, list_entries, view_entry_alignment
 
 urlpatterns = [
-    path('', view_entry),
-    path('demo', demo_entry),
+    path('', list_entries, name='list_entries'),
+    path('demo', demo_entry, name='demo_entry'),
+    path('<str:entry_id>/', view_entry, name='view_entry'),
+    path('alignment/<str:entry_id>/', view_entry_alignment, name='view_entry_alignment')
 ]
--- project_lexicon/lexicon/views.py
+++ project_lexicon/lexicon/views.py
@@ -1,9 +1,27 @@
+import pandas as pd
+
 from django.shortcuts import render
+
 
 
 def demo_entry(request):
     return render(request, 'lexicon/demo_entry.html')
 
 
-def view_entry(request):
-    return render(request, 'lexicon/view_entry.html')
+def list_entries(request):
+    lexicon = pd.read_csv('../data/alignment/dictionary.csv')
+    strongs = lexicon.strongs.value_counts().sort_index()
+    # strongs = sorted(strongs)
+    return render(request, 'lexicon/list_entries.html', {'entries': strongs})
+
+
+def view_entry(request, entry_id):
+    lexicon = pd.read_csv('../data/alignment/dictionary.csv')
+    entry = lexicon.loc[lexicon.strongs==entry_id]
+    return render(request, 'lexicon/view_entry.html', {'entry':entry_id })
+
+
+def view_entry_alignment(request, entry_id):
+    lexicon = pd.read_csv('../data/alignment/dictionary.csv')
+    entry = lexicon.loc[lexicon.strongs==entry_id]
+    return render(request, 'lexicon/view_entry_alignment.html', {'entry':entry.to_html(index=False) })
