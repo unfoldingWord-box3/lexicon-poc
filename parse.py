800fd55dfd0792e15b41f3652ac0f4736ed826f7
J. de Joode <10449733+jdejoode@users.noreply.github.com>
2020-11-13 08:59:08 +0100

    Add colors, icons, samples, and senses to view_entry

--- parse.py
+++ parse.py
@@ -1,156 +1,15 @@
 
-
-'''
-Description of the format of the data
-=====================================
-
-There are four categories of metadata: 
-
-1. key-values such as `\ide UTF-8` and `\h 1 Kings`
-2. Other items have opening and closing tags:
-    \w ... \w*        # tag is repeated
-    \zaln-s ... \*    # opened then the attributes follow, then closed
-    \zaln-e\*         # opened and closed simultaneously
-3. Some items are not surrounded by a tag, such as punctuation.
-4. Some tags have no values: \s5 \p
-
-Footnotes are in the following shape: \f + \ft Some versions have, \fqa the ark of Yahweh  \fqa* . \f* 
-Whitespace is not meaningful.
-
-Attributes are specified after a `|` in the format `attr="value"`. 
-Attributes can be separated by spaces. 
-In this set of USFM documents all attributes have `x-*` prepended.
-
-The alignment data is structured following these principles
-
-1. Linking: Embedding a target language word within a a zalg-start/zalg-end pair means 
-    that the target language word is aligned with the source language word
-2. Grouping: Nesting multiple source language words combines them in a group.
-    Embedding multiple target language words groups them.
-3. Gapping: To refer to words that have a gap one can refer to their x-occurrence. 
-    If two alignments refer to the same source language occurrence, they form a single 
-    alignment. This principle is implicit.
-    
-\\usfm either ends with a linebreak OR with another key_value tag
-'''
-
 import re
+import sys
+import os
+
 import pandas as pd
+import spacy
 
-# with open('./data/en_ult/18-JOB.usfm') as ipf: 
-# with open('./data/en_ult/01-GEN.usfm') as ipf: 
-with open('./data/en_ult/29-JOL.usfm') as ipf: 
-# with open('./data/alignment-example.usfm') as ipf: 
-# with open('./data/en_ult/41-MAT.usfm') as ipf: 
-# with open('./data/el-x-koine_ugnt/41-MAT.usfm') as ipf: 
-    contents = ipf.read()
+df_full = pd.read_csv('data/alignment/ult.csv')
 
-print('There are {} tokens in the sample.'.format(len(contents.split())))
+# fields to extract: occ, occs, nrOfTargetWords, nrOfSourceWords
 
-set(re.findall(r'\\.*?\s', contents, re.DOTALL)) 
-
-# These can be bound be a space or a newline, so really by the next tag
-KEY_VALUE = [
- '\\c ',
- '\\id ',
- '\\ide ',
- '\\mt ',
- '\\toc1 ',
- '\\toc2 ',
- '\\toc3 ',
- '\\usfm ',
- '\\h ',
- '\\v ',
-]
-
-# these can be embedded
-OPEN_CLOSING = [
- ('\\zaln-s', '\\*'),
- ('\\zaln-e', '\\*'),
- ('\\k-s', '\\*'),
- ('\\k-e', '\\*'),
- ('\\w ', '\\w* '),
- ('\\f ', '\\f* '),
- ('\\fqa ', '\\fqa* '), # Footnote translation quotation alternative
-]
-
-SWITCHES = [
- '\\p ',
- '\\s5 ',
- '\\q ',  # quotes end at the end of the sentence, this is implicit
- '\\q2 ',
- '\\ft ', # "essential (explanatory) text of the footnote", also implicitly closed
-]
-
-'''
-Multiple options:
-1. Create a state machine that parses token per token and keeps track of its embedding.
-    Such a model is a lexer-parser type of model.
-2. Parse the main building blocks such as the header, the chapters, and sections as these
-are all relatively predictable. Then parse each chunk of text and extract the alignments.
-Put these in a pandas dataframe and extract the data at token level.
-3. Encode the data as a tree
-
-I here opt for model 2. 
-'''
-
-chapters = contents.split('\c ')
-header = chapters[0]
-header = ' '.join(header.split())  # remove linebreaks because they are untrustworthy
-chapters.pop(0)  # remove the header
-
-HEADER = {}
-for key in KEY_VALUE:
-    # find the key and select everything until the next backslash
-    value = re.findall(r'\{}[^\\]*'.format(key), header)
-    # only select the first item of the resuls list
-    try: 
-        HEADER[key.strip().strip('\\')] = value[0].strip().lstrip(key)
-    except IndexError:
-        print(f'For {key} there is no value')
-print(HEADER)
-
-# parse the remainder of the chapters, 
-# extract the words, and their alignment data
-words = []
-i = 1
-
-for chapter in chapters:
-    chapter_nr = re.findall(r'^\d+', chapter)[0]
-    chapter = chapter.lstrip(chapter_nr).strip()
-    
-    # remove the switches
-    for switch in SWITCHES:
-        chapter = chapter.replace(switch, '')
-        chapter = ' '.join(chapter.split())  # remove left-over linebreaks
-
-    verses = chapter.split('\\v ')
-    # remove the part before the first verse
-    verses.pop(0)
-    for verse in verses:
-        verse_nr = re.findall(r'^\d+', verse)[0]
-        verse = verse.lstrip(verse_nr).strip()
-        verse = ' '.join(verse.split())  # remove left-over linebreaks
-        if 'zaln-s' in verse:
-            alignments = verse.split('\\zaln-e\\*')
-            for alignment in alignments:
-                align_data = re.findall(r'\\zaln-s.*?\*', alignment) 
-                raw_word_data = re.sub(r'\\zaln-s.*?\*', '', alignment)
-                for word in raw_word_data.split('\w*'): 
-                    words.append({'token': word, 'alg': align_data, 'alg_id':i, 'chapter':chapter_nr, 'verse':verse_nr})
-                i += 1
-        elif '\w' in verse:
-            raw_words = verse.split('\w*')
-            for word in raw_words:
-                words.append({'token': word, 'chapter':chapter_nr, 'verse':verse_nr})
-        else: 
-            raw_words = verse.split()
-            for word in raw_words:
-                words.append({'token': word, 'chapter':chapter_nr, 'verse':verse_nr})
-
-# Process the data 
-df_full = pd.DataFrame(words)
-print(df_full)
 
 ## get source text phrases
 source_blocks = df_full.groupby('alg_id').first()
@@ -167,7 +26,7 @@ df = df_full.loc[df_full.alg.isnull()==False,:]
 # df = df.loc[df.token != '',:]
 
 # Change float to int 
-df['alg_id'] = df['alg_id'].astype(int)
+# df['alg_id'] = df['alg_id'].astype(int)
 
 ## There are cases where an alignment is interrupted
 # find cases where the alignment id is different, 
@@ -215,7 +74,7 @@ have_gaps = df.loc[df.alg_id.isin(merging.values())].groupby('alg_id').apply(lam
 need_gap_highlighting = [i for itm in have_gaps.tolist() for i in itm]
 df.loc[:,'alg_has_gap'] = False
 df.loc[need_gap_highlighting, 'alg_has_gap'] = True
-df.to_excel('./data/alignment/target.xlsx')  # CAREFUL: this only exports THOSE TOKENS WITH AN ALIGNMENT
+# df.to_excel('./data/alignment/target.xlsx')  # CAREFUL: this only exports THOSE TOKENS WITH AN ALIGNMENT
 df.to_csv('./data/alignment/target.csv')  # CAREFUL: this only exports THOSE TOKENS WITH AN ALIGNMENT
 
 # do this after so the export above is still clear
@@ -236,7 +95,7 @@ df.loc[:, 'hasPrefix'] = False
 df.loc[df.strongs.str.contains(r'd:|c:|l:|i:|k:|b:|m:').fillna(False), 'hasPrefix'] = True
 df.loc[:, 'strongsNoPrefix'] = df.strongs.str.strip('d:|c:|l:|i:|k:|b:|m:')
 
-df.to_excel('./data/alignment/alignment.xlsx')
+## df.to_excel('./data/alignment/alignment.xlsx')
 df.to_csv('./data/alignment/alignment.csv')
 
 # Do some analysis
@@ -252,19 +111,45 @@ source_blocks = df.groupby('strongs alg_id'.split()).source_blocks.first()
 
 allinone = pd.concat([target_tokens, lemmata, source_tokens, source_blocks], axis=1)
 allinone.rename(columns={0:'target_blocks'}, inplace=True)
+
+# choose a root for each alignment
+nlp = spacy.load("en_core_web_sm")
+
+def parse(input_string):
+    output = []
+    doc = nlp(input_string)
+    for token in doc:
+        output.append([token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+                token.shape_, token.is_alpha, token.is_stop])
+    return pd.DataFrame(output, columns='token lemma pos tag dep shape is_alpha is_stop'.split())
+
+roots = []
+for itm in allinone.target_blocks.tolist():
+    parsing = parse(itm)
+    # print()
+    # print(parsing.loc[parsing.dep == 'ROOT', 'lemma'].tolist(), itm)
+    roots.append(parsing.loc[parsing.dep == 'ROOT', 'lemma'].tolist()[0])
+allinone['roots'] = roots
+
 dense = allinone.groupby('strongs target_blocks'.split()).size()
 dense
 allinone.groupby('strongs target_blocks'.split()).size()
 allinone.groupby('strongs source_blocks target_blocks'.split()).size().head(60)
 
 allinone.to_csv('./data/alignment/dictionary.csv')
-allinone.to_excel('./data/alignment/dictionary.xlsx')
+## allinone.to_excel('./data/alignment/dictionary.xlsx')
 
 # get the counts for each sense
 counts = allinone.groupby(['strongs', 'target_blocks']).size()
-counts.to_excel('./data/alignment/counts.xlsx')
+## counts.to_excel('./data/alignment/counts.xlsx')
 counts.to_csv('./data/alignment/counts.csv')
 counts.head(50)
+
+
+
+
+
+
 
 # query a strongs number
 allinone.loc['G00180']
@@ -297,6 +182,10 @@ df.loc[df.alg.str.get(0).fillna('').str.contains('H1121a')].target_token.str.upp
 
 df.loc[df.strongsNoPrefix == 'H0559'].target_token.str.upper().value_counts()
 df.loc[-df.hasPrefix]
+
+
+
+
 
 # IDEAS
 # we are going to find a lot of alignment mistakes, maybe even translation mistakes
--- project_lexicon/lexicon/views.py
+++ project_lexicon/lexicon/views.py
@@ -1,6 +1,31 @@
+import json
 import pandas as pd
+from collections import OrderedDict
 
 from django.shortcuts import render
+
+
+COLOR_SCALE = {1:'darken-4',
+           2:'darken-3',
+           3:'darken-2',
+           4:'lighten-1',
+           5:'lighten-2',
+           6:'lighten-3',
+           7:'lighten-4',
+           8:'lighten-5',
+           9:'lighten-5',
+           10:'lighten-5',
+}
+for i in range(11,5000): COLOR_SCALE[i] = 'lighten-5'
+
+ICON_SCALE = {
+    1:'looks_one',
+    2:'looks_two',
+    3:'looks_3',
+    4:'looks_4',
+    5:'looks_5',
+    6:'looks_6' }
+for i in range(7,5000): ICON_SCALE[i] = ''
 
 
 def demo_entry(request):
@@ -9,21 +34,49 @@ def demo_entry(request):
 
 def list_entries(request):
     lexicon = pd.read_csv('../data/alignment/dictionary.csv')
-    strongs = lexicon.strongs.value_counts().sort_index()
-    # strongs = sorted(strongs)
-    return render(request, 'lexicon/list_entries.html', {'entries': strongs})
+    # all_numbers = json.dumps({nr:nr for nr in lexicon.strongs.unique().tolist()})
+    all_numbers = json.dumps(lexicon.strongs.unique().tolist())
+    strongs = lexicon.strongs.value_counts().sort_index().sample(50)
+    return render(request, 'lexicon/list_entries.html', {'entries': strongs, 'all_numbers':all_numbers})
 
 
 def view_entry(request, entry_id):
     lexicon = pd.read_csv('../data/alignment/dictionary.csv')
+    ALIGNMENT = pd.read_csv('../data/alignment/alignment.csv')
     entry = lexicon.loc[lexicon.strongs==entry_id]
     lemma = entry.loc[:,'lemma'].iloc[0]
     font = entry_id[0]
     if font=='A': font='H'
+
+    senses_text = entry.groupby('roots').size().sort_values(ascending=False).index.tolist()
+    
+    senses = []
+    frequencies = entry.groupby('target_blocks').size().sort_values(ascending=False)
+
+    for idx,freq in enumerate(frequencies.items(), start=1):
+        # order, color, icon, frequency, 5 samples      
+        sense, frequency = freq[0].strip(), freq[1]
+        examples_idx = ALIGNMENT.loc[(ALIGNMENT.strongsNoPrefix==entry_id) & (ALIGNMENT.target_token.str.strip()==sense)].index.tolist()
+        samples = []
+        for example in examples_idx[:5]:
+            case = ALIGNMENT.loc[example-7:example+7]
+            case.loc[example, 'target_token'] = '<span class="hl">' + case.loc[example, 'target_token'] + '</span>'
+            samples.append(''.join(case.target_token.fillna('').tolist()))
+
+        senses.append({'freq':frequency,
+                       'sense':sense,
+                       'color':COLOR_SCALE[idx],
+                       'order':idx,
+                       'icon':ICON_SCALE[idx],
+                       'samples':samples,
+        })
+
     return render(request, 'lexicon/view_entry.html', 
         {'entry':entry_id,
          'lemma': lemma,
-         'font': font
+         'font': font,
+         'senses_text': senses_text,
+         'senses': senses,
           })
 
 
