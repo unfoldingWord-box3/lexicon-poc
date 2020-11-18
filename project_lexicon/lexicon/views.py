72449fe98ff85087dacb47cad8a0d0e4935660ce
J. de Joode <10449733+jdejoode@users.noreply.github.com>
2020-11-18 12:12:50 +0100

    Update display

--- project_lexicon/lexicon/views.py
+++ project_lexicon/lexicon/views.py
@@ -1,8 +1,13 @@
 import json
-import pandas as pd
+import random
 from collections import OrderedDict
 
+import pandas as pd
+
 from django.shortcuts import render
+from django.db.models import Count
+
+from .models import Source, Target, Alignment, StrongsM2M
 
 
 COLOR_SCALE = {1:'darken-4',
@@ -33,6 +38,125 @@ def demo_entry(request):
 
 
 def list_entries(request):
+    sources = Source.objects.values('strongs_no_prefix', 'lemma').annotate(count=Count('strongs_no_prefix'))
+    strongs = {itm['strongs_no_prefix']:str(itm['count']) + 'x ({})'.format(itm['lemma']) for itm in sources[:50] if itm['count']}
+    all_numbers = None
+    return render(request, 'lexicon/list_entries.html', {'entries': strongs, 'all_numbers':all_numbers})
+
+
+def get_concordance(alg_ids, idx, alignment_df, model, id_column, token_column, token_prefix_column=None, window=4):
+    samples = []
+    samples_ids = {}
+    sample_highlights = []
+    for id in alg_ids:
+        selection = alignment_df.loc[alignment_df.id==id, id_column].tolist()
+        start = min(selection) - window
+        end = max(selection) + window
+        samples_ids[idx] = list(range(start,end))
+        sample_highlights.extend(selection)
+            
+        ids = [i for itm in samples_ids.values() for i in itm]
+        if token_prefix_column:
+            s = model.objects.values(token_column, token_prefix_column, 'id').filter(id__in=ids)
+        else:
+            s = model.objects.values(token_column, 'id').filter(id__in=ids)
+        s = pd.DataFrame(s)
+        if token_prefix_column:
+            s[token_column] = s[token_prefix_column] + s[token_column]
+        s.loc[s.id.isin(sample_highlights), token_column] = '<span class="hl">'+s[token_column]+'</span>'
+        
+        for key,val in samples_ids.items():
+            samples.append(''.join(s.loc[s.id.isin(val)][token_column].fillna('').tolist()))
+    return samples
+                
+
+def get_font(entry_id):
+    if entry_id[0]=='A': 
+        font='hb'
+    elif entry_id[0]=='H': 
+        font='hb'
+    elif entry_id[0]=='G': 
+        font='gk'
+    else:
+        font=None
+    return font
+
+
+def view_entry(request, entry_id):
+    aligs = Alignment.objects.filter(source__strongs_no_prefix=entry_id)
+    lemma = Source.objects.filter(strongs_no_prefix=entry_id).first().lemma
+    font = get_font(entry_id)
+
+    alignments = pd.DataFrame(aligs.values())
+    frequencies = alignments.drop_duplicates('alg_id').groupby('target_blocks').size().sort_values(ascending=False)
+    
+    # senses_text = alignments.groupby('roots').size().sort_values(ascending=False).index.tolist()
+    senses_text = alignments.groupby('target_blocks').size().sort_values(ascending=False).index.tolist()
+
+    senses = []
+    for idx,freq in enumerate(frequencies.items(), start=1):
+        # order, color, icon, frequency, 5 samples      
+        sense, frequency = freq[0].strip(), freq[1]
+
+        alg = alignments.loc[alignments.target_blocks==freq[0]]
+        alg_ids = alg.id.unique().tolist()
+        if len(alg_ids) > 5:
+            alg_ids = random.sample(alg_ids, k=5)
+
+        target_samples = get_concordance(alg_ids, idx, alignments, Target, 'target_id', 'target_token', 'target_token_prefix', window=8)
+        source_samples = get_concordance(alg_ids, idx, alignments, Source, 'source_id', 'token')
+
+        # target_samples_ids = {}
+        # target_sample_hls = []
+        # for id in alg_ids:
+        #     selection = alignments.loc[alignments.id==id, 'target_id'].tolist()
+        #     # source_selection = selection.source_id.tolist()
+        #     start = min(selection) - 8
+        #     end = max(selection) + 8
+        #     target_samples_ids[idx] = list(range(start,end))
+        #     target_sample_hls.extend(selection)
+              
+        #     ids = [i for itm in target_samples_ids.values() for i in itm]
+        #     s = Target.objects.values('target_token', 'target_token_prefix', 'id', 'alg_id').filter(id__in=ids)
+        #     s = pd.DataFrame(s)
+        #     s['target_token'] = s['target_token_prefix'] + s['target_token']
+        #     s.loc[s.id.isin(target_sample_hls), 'target_token'] = '<span class="hl">'+s.target_token+'</span>'
+            
+        #     for key,val in target_samples_ids.items():
+        #         target_samples.append(''.join(s.loc[s.id.isin(val)].target_token.fillna('').tolist()))
+                
+        senses.append({'freq':frequency,
+                       'sense':sense,
+                       'color':COLOR_SCALE[idx],
+                       'order':idx,
+                       'icon':ICON_SCALE[idx],
+                       'target_samples':target_samples,
+                       'source_samples':source_samples,
+        })
+    
+    #TODO related items
+    # - show page when no alignment data
+    # - fix partial strongs numbers in Greek (G...)
+    # - use proper fonts
+    related_items = StrongsM2M.objects.filter(number=entry_id).values_list('related_number')
+    strongs = Source.objects.filter(strongs_no_prefix__in=related_items).values_list('strongs_no_prefix', 'lemma')
+    print(strongs)
+    related_items = dict(strongs)
+
+   
+    #TODO merge senses
+
+    return render(request, 'lexicon/view_entry.html', 
+        {'entry':entry_id,
+         'lemma': lemma,
+         'font': font,
+         'senses_text': senses_text,
+         'senses': senses,
+         'related_items': related_items,
+          })
+
+
+def list_entries_df(request):
     lexicon = pd.read_csv('../data/alignment/dictionary.csv')
     # all_numbers = json.dumps({nr:nr for nr in lexicon.strongs.unique().tolist()})
     all_numbers = json.dumps(lexicon.strongs.unique().tolist())
@@ -40,13 +164,12 @@ def list_entries(request):
     return render(request, 'lexicon/list_entries.html', {'entries': strongs, 'all_numbers':all_numbers})
 
 
-def view_entry(request, entry_id):
+def view_entry_df(request, entry_id):
     lexicon = pd.read_csv('../data/alignment/dictionary.csv')
     ALIGNMENT = pd.read_csv('../data/alignment/alignment.csv')
     entry = lexicon.loc[lexicon.strongs==entry_id]
     lemma = entry.loc[:,'lemma'].iloc[0]
-    font = entry_id[0]
-    if font=='A': font='H'
+    font = get_font(entry_id)
 
     senses_text = entry.groupby('roots').size().sort_values(ascending=False).index.tolist()
     
