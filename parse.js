6cb84dde19488b30beba6dc7978b9f6adcdb6602
J. de Joode <10449733+jdejoode@users.noreply.github.com>
2021-01-06 10:55:09 +0100

    Use proskomma to parse GL quotes

--- parse.js
+++ parse.js
@@ -2,7 +2,8 @@ var usfm = require('usfm-js');
 var fs = require('fs');
 
 try {
-    const sample = fs.readFileSync('data/el-x-koine_ugnt/41-MAT.usfm', 'utf8')
+    // const sample = fs.readFileSync('data/el-x-koine_ugnt/41-MAT.usfm', 'utf8')
+    const sample = fs.readFileSync('data/en_ult/01-GEN.usfm', 'utf8')
     var toJSON = usfm.toJSON(sample);
     //  console.log(sample)
 } catch (err) {
@@ -11,4 +12,5 @@ try {
 
 //Convert from USFM to JSON
 // console.log(toJSON)
-console.log(toJSON['chapters']['1']['18'])
+// console.log(JSON.stringify(toJSON['chapters']['1']['18']))
+console.log(JSON.stringify(toJSON))
