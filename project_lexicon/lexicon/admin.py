cbb5c14332ec253ae4dba5e2d900b2a7a4c276ce
J. de Joode <10449733+jdejoode@users.noreply.github.com>
2021-01-28 11:32:41 +0100

    Activate admin CRUD interface

--- project_lexicon/lexicon/admin.py
+++ project_lexicon/lexicon/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+
+#  https://medium.com/hackernoon/automatically-register-all-models-in-django-admin-django-tips-481382cf75e5
+
+
+class ListAdminMixin(object):
+    def __init__(self, model, admin_site):
+        self.list_display = [field.name for field in model._meta.fields]
+        super(ListAdminMixin, self).__init__(model, admin_site)
+
+
+models = apps.get_models()
+for model in models:
+    admin_class = type('AdminClass', (ListAdminMixin, admin.ModelAdmin), {})
+    try:
+        admin.site.register(model, admin_class)
+    except admin.sites.AlreadyRegistered:
+        pass
