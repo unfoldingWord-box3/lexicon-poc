3dcafa9d6c2fb5d2a62af4b1fe57759331d6ec08
J. de Joode <10449733+jdejoode@users.noreply.github.com>
2021-03-04 15:25:56 +0100

    Add questions to the database and the API

---
+++ sql/questions2sql.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import glob
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+
+inputfiles = glob.glob('../data/en_tq/*.tsv')
+
+dataframes = []
+
+for ipf in inputfiles:  
+    df = pd.read_csv(ipf, sep='\t')
+    basename = os.path.basename(ipf).replace('_tq.tsv', '')
+    df['book'] = [basename] * df.shape[0]
+    chapter_verse = df.Reference.str.split(':', expand=True)
+    df['chapter'] = chapter_verse[0].astype(int)
+    df['verse'] = chapter_verse[1].astype(int)
+    dataframes.append(df)
+
+df = pd.concat(dataframes)
+df.reset_index(inplace=True, drop=True)
+
+df.to_csv('../data/csv/tq.csv')
+
+engine = create_engine('sqlite:///../project_lexicon/alignment.db', echo=False)
+df.to_sql('question', con=engine, if_exists='replace')
--- sql/run.sh
+++ sql/run.sh
@@ -4,6 +4,7 @@ python notes2sql.py
 python words2sql.py
 python lexicon2sql.py
 python glosses2sql.py
+python questions2sql.py
 python ../project_lexicon/manage.py migrate
 python create_indexes.py
 # This command is last so it can fail if the db already is up and running
